1c302f3d9916eaa77f984eb276ab55cfbae53b1b
Pablo Tesone <tesonep@gmail.com>
2022-10-03 17:07:50 +0200

    Recovering push.sh

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 echo Uploading talk files to files.pharo.org...
 #rsync --progress --cvs-exclude --recursive --links --times --delete .  filepharosync@files.pharo.org:/appli/files.pharo.org/talks
-rsync  --progress --exclude .git/ --exclude .gitignore --recursive --links --times --delete  . files.pharo.org:talks
+rsync  --progress --exclude .git/ --exclude .gitignore --recursive --links --times --delete  . files.pharo:talks
 echo done
 
 
